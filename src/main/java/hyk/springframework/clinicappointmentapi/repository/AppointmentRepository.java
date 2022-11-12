7a3bf6219dd925beca5b1ebed89dede577ee6927
Yanant27 <htooyanant.hyk@gmail.com>
2022-11-12 20:02:11 +0630

    Modify repositories.

--- src/main/java/hyk/springframework/clinicappointmentapi/repository/AppointmentRepository.java
+++ src/main/java/hyk/springframework/clinicappointmentapi/repository/AppointmentRepository.java
@@ -2,19 +2,16 @@ package hyk.springframework.clinicappointmentapi.repository;
 
 import hyk.springframework.clinicappointmentapi.domain.Appointment;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-import java.time.LocalDate;
 import java.util.List;
-import java.util.Optional;
 
 /**
  * @author Htoo Yanant Khin
  **/
-public interface AppointmentRepository extends JpaRepository<Appointment, Long>, JpaSpecificationExecutor<Appointment> {
+public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
     List<Appointment> findAllByDoctorId(Long doctorId);
 
-    List<Appointment> findAllByPatientId(Long patientid);
+    List<Appointment> findAllByPatientId(Long patientId);
 
     List<Appointment> findAllByScheduleId(Long scheduleId);
 }
--- src/main/java/hyk/springframework/clinicappointmentapi/repository/ScheduleRepository.java
+++ src/main/java/hyk/springframework/clinicappointmentapi/repository/ScheduleRepository.java
@@ -4,10 +4,13 @@ import hyk.springframework.clinicappointmentapi.domain.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * @author Htoo Yanant Khin
  **/
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
     List<Schedule> findAllByDoctorId(Long doctorId);
+
+    Optional<Schedule> findByIdAndDoctorId(Long scheduleId, Long doctorId);
 }
