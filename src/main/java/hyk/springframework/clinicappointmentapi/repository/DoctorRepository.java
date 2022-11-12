eddcdc9ab01581d83b98d1feca948a3e9c7fed88
Yanant27 <htooyanant.hyk@gmail.com>
2022-11-12 20:13:45 +0630

    Update doctor repository.

--- src/main/java/hyk/springframework/clinicappointmentapi/repository/DoctorRepository.java
+++ src/main/java/hyk/springframework/clinicappointmentapi/repository/DoctorRepository.java
@@ -3,8 +3,11 @@ package hyk.springframework.clinicappointmentapi.repository;
 import hyk.springframework.clinicappointmentapi.domain.Doctor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 /**
  * @author Htoo Yanant Khin
  **/
 public interface DoctorRepository extends JpaRepository<Doctor, Long> {
+    List<Doctor> findAllBySpecialization(String specialization);
 }
